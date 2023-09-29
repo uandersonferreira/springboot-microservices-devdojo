9a2725f14c2d0f7ebfcec80df93525468a99b703
Uanderson Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2023-09-29 15:26:55 -0300

    feat: create module course

---
+++ course/src/test/java/br/com/uanderson/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uanderson.course;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
